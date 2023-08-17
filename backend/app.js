5ba44fcd9c21655ba5a976cb0aa5a27f609a9cd0
ADEP-123 <andep210399@gmail.com>
2023-08-17 07:26:50 -0500

    feat: :construction: Trabajando en Enrutador

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,8 @@
 import express from 'express';
-
-// import initApiRoutes from './src/routes/routes.js';
+import initApiRoutes from './src/routes/routes.js';
 
 const appExpress = express();
 appExpress.use(express.json())
-appExpress.use('/campus', (req, res, next) => {
-    res.json({ Hola: "Holaa" })
-});
+appExpress.use('/campus', initApiRoutes());
 
 export default appExpress;
---
+++ backend/src/routes/routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+// import getInitRoute from "./getData.js";
+// import postInitRoute from "./postData.js";
+// import putInitRoute from "./putData.js";
+// import { middlewareRateLimit } from "../middleware/limit.js";
+// import { appToken } from "../services/tokenGenerator.js";
+// import { authorizationMiddleware } from "../middleware/autohorizationMiddleware.js";
+
+const initApiRoutes = () => {
+    const router = Router();
+    router.use("/test", (req,res,next)=>{
+        res.json({res: "Funcionando"})
+    })
+    // router.use("/login", appToken)
+    // router.use("/get", getInitRoute())
+    // router.use("/post", postInitRoute())
+    // router.use("/put", putInitRoute())
+    return router
+}
+
+export default initApiRoutes
