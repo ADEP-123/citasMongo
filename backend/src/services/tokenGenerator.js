a2aecfac4734fcdd81081e2d792092d21c014b8e
ADEP-123 <andep210399@gmail.com>
2023-08-20 09:58:09 -0500

    feat: :sparkles: Servicio para generar token de acceso

---
+++ backend/src/services/tokenGenerator.js
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import { plainToClass, classToPlain } from 'class-transformer';
+import dotenv from 'dotenv';
+import { Router } from 'express';
+import { SignJWT } from 'jose';
+
+
+dotenv.config();
+const appToken = Router();
+
+appToken.use("/:collecion", async (req, res) => {
+    try {
+        let inst = plainToClass(eval(req.params.collecion), {}, { ignoreDecorators: true })
+        const encoder = new TextEncoder();
+        const jwtconstructor = new SignJWT(Object.assign({}, classToPlain(inst)));
+        const jwt = await jwtconstructor
+            .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+            .setIssuedAt()
+            .setExpirationTime("30m")
+            .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+        req.data = jwt;
+        res.status(201).send({ status: 201, message: jwt });
+    } catch (error) {
+        console.log(error);
+        res.status(404).send({ status: 404, message: "Token solicitado no valido" });
+    }
+})
+
+export {
+    appToken,
+};
