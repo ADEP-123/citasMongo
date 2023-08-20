81e7a866766187c3c8e1c66c2826b1d2ef5ae4b4
ADEP-123 <andep210399@gmail.com>
2023-08-20 11:15:16 -0500

    feat: :sparkles: DTOS consultas

---
+++ backend/src/routes/dto/js/pacientesMedico.js
@@ -0,0 +1,32 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { Expose, Transform } from "class-transformer";
+export class pacientesMedicoDTO {
+    constructor(data) {
+        Object.assign(this, data);
+    }
+}
+__decorate([
+    Expose({ name: "idMedico" }),
+    Transform(({ value, key }) => {
+        if (value) {
+            if (/^[0-9]{10}$/.test(value)) {
+                return value;
+            }
+            else {
+                throw { status: 400, message: `El documento de identidad del medico es requerido` };
+            }
+        }
+        else {
+            return value;
+        }
+    }, { toClassOnly: true }),
+    __metadata("design:type", String)
+], pacientesMedicoDTO.prototype, "cit_medico", void 0);
