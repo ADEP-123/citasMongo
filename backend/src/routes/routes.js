deb5c0265747d38a882348664fc8771e0761c56c
ADEP-123 <andep210399@gmail.com>
2023-08-21 11:56:51 -0500

    feat: :sparkles: Rate Limit middleware

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import getInitRoute from "./getData.js";
 import postInitRoute from "./postData.js";
-// import { middlewareRateLimit } from "../middleware/limit.js";
+import { middlewareRateLimit } from "../middleware/limit.js";
 import { appToken } from "../services/tokenGenerator.js";
 import { authorizationMiddleware } from "../middleware/authorizationMiddleware.js";
 
 
 const initApiRoutes = () => {
     const router = Router();
-    router.use("/login", appToken)
-    router.use("/get", authorizationMiddleware, getInitRoute())
-    router.use("/post", authorizationMiddleware, postInitRoute())
+    router.use("/login", middlewareRateLimit, appToken)
+    router.use("/get", middlewareRateLimit, authorizationMiddleware, getInitRoute())
+    router.use("/post", middlewareRateLimit, authorizationMiddleware, postInitRoute())
     return router
 }
 
