33f8b72ff51d8e814f81dcf3ff75a211c017c3e0
ADEP-123 <andep210399@gmail.com>
2023-08-21 08:45:37 -0500

    feat: :sparkles: Middleware endpoint 7

--- backend/src/middleware/middlewaresDTO.js
+++ backend/src/middleware/middlewaresDTO.js
@@ -4,6 +4,7 @@ import { medicoEspecialidadDTO } from '../routes/dto/js/medicoEspecialidad.js';
 import { proximaCitaDTO } from '../routes/dto/js/proximaCita.js';
 import { pacientesMedicoDTO } from '../routes/dto/js/pacientesMedico.js';
 import { citaPacienteDTO } from '../routes/dto/js/citaPaciente.js';
+import { citaFechaDTO } from '../routes/dto/js/citaFecha.js';
 
 
 const middlewareMedicoSepecialidadDTO = ((req, res, next) => {
@@ -46,10 +47,21 @@ const middlewareCitaPacienteDTO = ((req, res, next) => {
     }
 });
 
+const middlewareCitasFechaDTO = ((req, res, next) => {
+    try {
+        let data = plainToClass(citaFechaDTO, req.query, { excludeExtraneousValues: true });
+        req.query = JSON.parse(JSON.stringify(data));
+        next()
+    } catch (err) {
+        res.status(err.status).send(err)
+    }
+});
+
 
 export {
     middlewareMedicoSepecialidadDTO,
     middlewareProximaCitaDTO,
     middlewarePacientesMedicoDTO,
-    middlewareCitaPacienteDTO
+    middlewareCitaPacienteDTO,
+    middlewareCitasFechaDTO
 }
