ddabe5a4f3ceee300ba98d4229776b119bab7cf9
ADEP-123 <andep210399@gmail.com>
2023-08-21 08:18:53 -0500

    feat: :sparkles: Middleware endpoint 6

--- backend/src/middleware/middlewaresDTO.js
+++ backend/src/middleware/middlewaresDTO.js
@@ -3,6 +3,7 @@ import { plainToClass } from 'class-transformer';
 import { medicoEspecialidadDTO } from '../routes/dto/js/medicoEspecialidad.js';
 import { proximaCitaDTO } from '../routes/dto/js/proximaCita.js';
 import { pacientesMedicoDTO } from '../routes/dto/js/pacientesMedico.js';
+import { citaPacienteDTO } from '../routes/dto/js/citaPaciente.js';
 
 
 const middlewareMedicoSepecialidadDTO = ((req, res, next) => {
@@ -35,9 +36,20 @@ const middlewarePacientesMedicoDTO = ((req, res, next) => {
     }
 });
 
+const middlewareCitaPacienteDTO = ((req, res, next) => {
+    try {
+        let data = plainToClass(citaPacienteDTO, req.query, { excludeExtraneousValues: true });
+        req.query = JSON.parse(JSON.stringify(data));
+        next()
+    } catch (err) {
+        res.status(err.status).send(err)
+    }
+});
+
 
 export {
     middlewareMedicoSepecialidadDTO,
     middlewareProximaCitaDTO,
-    middlewarePacientesMedicoDTO
+    middlewarePacientesMedicoDTO,
+    middlewareCitaPacienteDTO
 }
